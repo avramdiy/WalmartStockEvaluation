8e64f8b5e4fe6d763297f2d9398bd58fe7e72ad5
Avram Diyaljee <avram.diyaljee@gmail.com>
2025-02-27 13:12:54 -1000

    1st Commit, see README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# TRG Week 13
+
+## Walmart Stock Evaluation
+
+- Link to Kaggle Dataset : https://www.kaggle.com/datasets/meharshanali/walmart-stocks-data-2025
+
+### 1st Commit
+
+Initiated data.py to load HTML dataframe, README.md to document commits, table.html to load initial Dataframe
+
+### 2nd Commit
+
+### 3rd Commit
+
+### 4th Commit
+
+### 5th Commit
---
+++ app/data.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__, template_folder=os.path.abspath("templates"))
+
+
+@app.route("/")
+def load_dataframe():
+    # Path to the data file
+    data_path = r"C:\Users\Ev\Desktop\TRG Week 13\walmart_stock_prices.csv"
+    
+    # Load data into a pandas DataFrame
+    df = pd.read_csv(data_path)
+    
+    # Convert DataFrame to HTML table
+    df_html = df.to_html(classes="table table-striped", index=False)
+    
+    # Pass the HTML table to the template
+    return render_template("table.html", table=df_html)
+
+if __name__ == "__main__":
+    app.run(debug=True)
